af2322e1bffdcef56fbaf51939702dde1e105c4b
ilef <ilefidoudi3@gmail.com>
2025-02-02 21:59:43 +0100

    Initial commit

---
+++ app/src/main/java/com/example/projetpfe/forget_password.kt
@@ -0,0 +1,16 @@
+package com.example.projetpfe
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class forget_password : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_forget_password)
+
+    }
+}
